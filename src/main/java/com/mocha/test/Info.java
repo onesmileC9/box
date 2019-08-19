5b64f371ff91aae39617104cdd383fb6d9c8cbb7
oneSmile <79837732@qq.com>
2019-08-19 11:00:16 +0800

    测试上传

---
+++ src/main/java/com/mocha/test/Info.java
@@ -0,0 +1,21 @@
+package com.mocha.test;
+
+/**
+ * @program: test
+ * @description:
+ * @author: 侯宇翔
+ * @create: 2019-08-19
+ **/
+public class Info {
+    public String test() {
+        String b = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+        //test
+        String a = "https://10.23.44.13:8080/file";
+        String password = "aaa";
+        return null;
+    }
+
+    protected void test2() {
+        String w = "passwordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpassword";
+    }
+}
